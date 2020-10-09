de9d4559c13a2b99faccee53a72d5ab8b7c91391
jokale <kalemajoanna@gmail.com>
2020-10-09 15:03:16 +0100

    create form skeleton

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,7 +6,17 @@ class Contact extends React.Component {
     render(){
         return(
             <div>
-                <h2>Contact</h2>
+                <h2>Contact Us</h2>
+                <center>
+                <form>
+                    <label>Name</label> <br></br>
+                    <input></input> <br></br>
+                    <label>Email</label> <br></br>
+                    <input></input><br></br>
+                    <label>Message</label><br></br>
+                    <input type="text-area"></input><br></br>
+                    <button type="submit">Submit</button><br></br>
+                </form></center>
             </div>
         )
     }
