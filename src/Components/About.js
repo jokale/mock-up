1def17eeebf2a08ab002d068c5c4224ff4707439
jokale <kalemajoanna@gmail.com>
2020-10-09 15:15:02 +0100

    fix about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,13 +8,14 @@ class About extends React.Component {
             <div>
            <h1 id="About">Thee Inkers</h1>
 
-                <h2>About Us</h2>
+                <br></br>
+                <h2 id="abouth2">About Us</h2> <br></br>
 
                 <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac lorem bibendum, facilisis sem id, aliquet mauris. Ut in arcu id justo porttitor sollicitudin. Aenean sed est sapien. Donec sit amet pellentesque nulla, vehicula pretium tortor. Quisque id erat ultricies, interdum dolor sit amet, ultrices erat. Nunc pellentesque pulvinar fringilla. In lectus eros, rutrum ut nunc sed, faucibus semper sem. Duis ut efficitur ligula, id efficitur odio. Quisque viverra euismod condimentum. Mauris at consectetur libero, at volutpat nisi. Vestibulum at odio et enim ultrices interdum. Morbi urna nisi, gravida a nisl id, condimentum vehicula quam. Maecenas suscipit consequat tortor vel elementum. Pellentesque porttitor nunc eget varius ultricies.
 
-                Proin eu urna eget lorem dictum tempor id id mauris. Praesent eget risus aliquam, posuere purus id, aliquam metus. Duis pretium cursus vehicula. Quisque porta velit ex, pulvinar tempor mauris fermentum at. Donec et vulputate est, dignissim pretium ipsum. Phasellus nibh leo, mattis at interdum at, congue sed odio. Fusce at neque scelerisque, eleifend urna sit amet, lobortis lectus. Mauris sodales, sem at rhoncus hendrerit, nunc massa iaculis mauris, a auctor mauris nunc ac ipsum. Fusce vestibulum erat eu lacinia facilisis. Morbi elementum interdum justo, eu rhoncus odio accumsan eu. Nullam vitae felis blandit leo hendrerit dictum. Maecenas laoreet nunc vitae massa elementum, et interdum elit gravida.</h5>
+                Proin eu urna eget lorem dictum tempor id id mauris. Praesent eget risus aliquam, posuere purus id, aliquam metus. Duis pretium cursus vehicula. Quisque porta velit ex, pulvinar tempor mauris fermentum at. Donec et vulputate est, dignissim pretium ipsum. Phasellus nibh leo, mattis at interdum at, congue sed odio. Fusce at neque scelerisque, eleifend urna sit amet, lobortis lectus. Mauris sodales, sem at rhoncus hendrerit, nunc massa iaculis mauris, a auctor mauris nunc ac ipsum. Fusce vestibulum erat eu lacinia facilisis. Morbi elementum interdum justo, eu rhoncus odio accumsan eu. Nullam vitae felis blandit leo hendrerit dictum. Maecenas laoreet nunc vitae massa elementum, et interdum elit gravida.
 
-                <h5>Pellentesque eget nunc ultricies, tristique diam nec, vestibulum est. Pellentesque quam ex, faucibus vitae molestie efficitur, dictum id turpis. Aenean pellentesque nunc rutrum elit interdum ullamcorper. Vestibulum tincidunt magna et dui euismod, non faucibus nunc ultrices.</h5>
+                <br></br>Pellentesque eget nunc ultricies, tristique diam nec, vestibulum est. Pellentesque quam ex, faucibus vitae molestie efficitur, dictum id turpis. Aenean pellentesque nunc rutrum elit interdum ullamcorper. Vestibulum tincidunt magna et dui euismod, non faucibus nunc ultrices.</h5>
               
             </div>
         )
