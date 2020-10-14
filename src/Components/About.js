8de1b7f7afc14439e07bcc5185605cdfc4baeff2
jokale <kalemajoanna@gmail.com>
2020-10-14 19:11:33 +0100

    add map to contact page n add styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -26,7 +26,6 @@ class About extends React.Component {
                 
                 
                 </p>
-                <iframe  id= "map"title="map" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4965.833892289461!2d-0.1038361!3d51.5147397!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1602696558812!5m2!1sen!2suk" width="600" height="450" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         )
     }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,16 +9,23 @@ class Contact extends React.Component {
                 <h1 id='contact'>Thee Inkers</h1>
                 <h2 >Contact Us</h2>
                 <center>
+                    <br></br><br></br>
                 <form>
-                    <br></br><label>Name</label> <br></br>
-                    <br></br><input></input> <br></br>
-                    <br></br><label>Email</label> <br></br>
-                    <br></br> <input></input><br></br>
-                    <br></br><label>Message</label><br></br>
-                    <textarea id="idea" name="w3review" placeholder='What is your idea?'rows="4" cols="50"/> <br></br>
-
+                    <br></br>
+                    
+                    <h4 id="tag1"><label>Name</label></h4><br></br>
+                    <br></br><input size ='70' placeholder='Name?'></input> <br></br>
+                    <br></br><h4 id="tag2"><label>Email</label></h4> <br></br>
+                    <br></br> <input size ='70' placeholder='Email?'></input><br></br>
+                    <br></br><h4 id="tag3"><label>Message</label></h4><br></br>
+                    <textarea id="idea" size='70' placeholder='What is your idea?'/> <br></br>
                     <button type="submit">Submit</button><br></br>
-                </form></center>
+                </form>
+                
+                <iframe  id= "map"title="map" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4965.833892289461!2d-0.1038361!3d51.5147397!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1602696558812!5m2!1sen!2suk" width="600" height="450" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+                
+                </center>
             </div>
         )
     }
