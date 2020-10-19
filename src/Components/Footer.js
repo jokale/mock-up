2c8a421ffbe4a0610be4073cc41914c842a268a5
jokale <kalemajoanna@gmail.com>
2020-10-19 21:00:47 +0100

    Add real links to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends React.Component {
         return(
             <div id="footer"> 
                 <br></br>
-                <a href="url">Instagram</a> <br></br>
-                <a href="url">Twitter</a><br></br><br></br>
-                <a href="url">Terms and Conditions </a><br></br>
-                <a href="url">Privacy policy </a>
+                <a href="https://www.instagram.com/">Instagram</a> <br></br>
+                <a href=" https://twitter.com/">Twitter</a><br></br><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/">Terms and Conditions </a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/">Privacy policy </a>
 
                 <br></br>
                 <h6>Joanna E Kalema Applications </h6>
