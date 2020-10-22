464a93f82cccefebbcb84597deabe1255c96fc39
jokale <kalemajoanna@gmail.com>
2020-10-22 11:53:20 +0100

    fix  footer errors

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends React.Component {
         return(
             <div id="footer"> 
                 <br></br>
-                <a href="https://www.instagram.com/"  target="_blank">Instagram</a> <br></br>
-                <a href=" https://twitter.com/"  target="_blank">Twitter</a><br></br><br></br>
-                <a href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/"  target="_blank">Terms and Conditions </a><br></br>
-                <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/"  target="_blank">Privacy policy </a>
+                <a href="https://www.instagram.com/"  target="_blank" rel="noopener noreferrer">Instagram</a> <br></br>
+                <a href=" https://twitter.com/"  target="_blank " rel="noopener noreferrer" >Twitter</a><br></br><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/"  target="_blank" rel="noopener noreferrer">Terms and Conditions </a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/"  target="_blank" rel="noopener noreferrer">Privacy policy </a>
 
                 <br></br>
                 <h6>Joanna E Kalema Applications </h6>
